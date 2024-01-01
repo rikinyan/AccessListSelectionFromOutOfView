a8587cc0d4406375c2ff66a608dec52ffcd630ad
Rikinyan <karasutengu0319@gmail.com>
2024-01-01 18:47:07 +0900

    Initial Commit

---
+++ AccessListSelectionFromOutOfView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessListSelectionFromOutOfView
+//
+//  Created by 力石優武 on 2024/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
